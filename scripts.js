6f91c5f9c68205fb7968481d86c105e04a6639c4
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-01-09 13:09:12 -0700

    Added Option for User to Input BPM Directly
    Because many people use metronomes to play to specific BPMs, I thought
    it would be a good idea to make sure that there was a way to input your
    own specific BPM. This is done with an input element that takes in values,
    then a button that initiates a function to update the BPM. This is handled
    by taking the value entered, making sure it is a valid entry, and then
    updating the display value, animationn time, and slider position.

--- scripts.js
+++ scripts.js
@@ -6,7 +6,30 @@ const BPMSlider = document.getElementById("BPMSlider");
 let BPM = document.getElementById("BPM");
 BPM.textContent = BPMSlider.value;
 
-// Update the output when the slider is moved
+// Variables for manual BPM input
+const manualBPMInput = document.getElementById("manualBPMInput");
+const manualBPMSet = document.getElementById("manualBPMSet");
+
+// Update BPM when set manually, ensure values are valid
+manualBPMSet.onclick = function () {
+    const newBPM = parseInt(manualBPMInput.value);
+
+    if (isNaN(newBPM) || newBPM < 40 || newBPM > 300) {
+        alert("Please input a valid BPM between 40-300 bpm");
+        return;
+    }
+
+    BPMSlider.value = newBPM;
+    BPM.textContent = newBPM;
+    let animationTime = 60 / newBPM;
+    document.getElementById("animation-container").style.animationDuration = `${animationTime}s`;
+}
+
+// Update the output when the slider is moved, update the animation
 BPMSlider.oninput = function() {
     BPM.textContent = this.value;
+    let animationTime = 60 / parseFloat(this.value); 
+    const animation = document.getElementById("animation-container")
+    animation.style.animationDuration = `${animationTime}s`
 };
+
